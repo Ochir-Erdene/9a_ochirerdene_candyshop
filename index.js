620b1736b1a331e2981bc4a7e86f78aa736b0b16
Ochir_Erdene <126749861+Ochir-Erdene@users.noreply.github.com>
2023-04-13 06:48:06 +0000

    good

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-let navbar = document.querySelector('.navbar');
-document.querySelector('#menu-bar').onclick=() =>{
-    navbar.classList.toggle('active');
-}
+let navbar = document.querySelector(".navbar");
+document.querySelector("#menu-bar").onclick = () => {
+  navbar.classList.toggle("active");
+};
 
-let search = document.querySelector('.search');
-document.querySelector('#search').onclick=() =>{
-    search.classList.toggle('active');
-}
+let search = document.querySelector(".search");
+document.querySelector("#search").onclick = () => {
+  search.classList.toggle("active");
+};
+
+var swiper = new Swiper(".product-row", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
